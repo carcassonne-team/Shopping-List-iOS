e32772cca0e26eae795514c703ce76ba3a14672d
Overnerfed <52073283+Overnerfed@users.noreply.github.com>
2021-03-17 19:30:55 +0100

    Add Login Coordinator

---
+++ Shopping List/Shopping List/LoginCoordinator.swift
@@ -0,0 +1,20 @@
+//
+//  LoginCoordinator.swift
+//  Shopping List
+//
+//  Created by Mateusz Leśniara on 17/03/2021.
+//
+
+import UIKit
+
+protocol LoginCoordinatorProtocol: CoordinatorProtocol { }
+ 
+final class LoginCoordinator: LoginCoordinatorProtocol {
+    func start() {
+        
+    }
+    
+    func didFinish(with child: CoordinatorProtocol?) {
+        
+    }
+}
