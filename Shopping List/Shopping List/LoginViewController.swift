0f5bee4c8c254a28182c44987c883b8bcaf30a7b
Overnerfed <52073283+Overnerfed@users.noreply.github.com>
2021-03-17 19:44:48 +0100

    Add LoginViewController

---
+++ Shopping List/Shopping List/LoginViewController.swift
@@ -0,0 +1,15 @@
+//
+//  LoginViewController.swift
+//  Shopping List
+//
+//  Created by Mateusz Leśniara on 17/03/2021.
+//
+
+import UIKit
+
+class LoginViewController: UIViewController {
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = .white
+    }
+}
