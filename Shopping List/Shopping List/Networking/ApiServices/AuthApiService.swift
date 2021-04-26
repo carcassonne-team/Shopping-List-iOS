8ac27caf1ab515a8d094ee4097561be230c8b4c9
Overnerfed <52073283+Overnerfed@users.noreply.github.com>
2021-04-27 01:07:21 +0200

    WIP Networking

---
+++ Shopping List/Shopping List/Networking/ApiServices/AuthApiService.swift
@@ -0,0 +1,64 @@
+//
+//  AuthApiService.swift
+//  Shopping List
+//
+//  Created by Mateusz Leśniara on 26/04/2021.
+//
+
+import Foundation
+import Moya
+
+enum AuthApiService {
+    case login(parameters: LoginParameters)
+    case register(parameters: RegisterParameters)
+}
+
+struct LoginParameters: Encodable {
+    let email: String
+    let password: String
+}
+
+struct RegisterParameters: Encodable {
+    let name: String
+    let email: String
+    let password: String
+    let passwordConfirmation: String
+}
+
+extension AuthApiService: TargetType {
+    var baseURL: URL {
+        URL(string: NetworkingConstants.baseURL + "/auth")!
+    }
+    
+    var path: String {
+        switch self {
+        case .login:
+            return "/login"
+        case .register:
+            return "/register"
+        }
+    }
+    
+    var method: Moya.Method {
+        return .post
+    }
+    
+    var sampleData: Data {
+        return Data()
+    }
+    
+    var task: Task {
+        let encoder = JSONEncoder()
+        encoder.keyEncodingStrategy = .convertToSnakeCase
+        switch self {
+        case .login(parameters: let parameters):
+            return .requestCustomJSONEncodable(parameters, encoder: encoder)
+        case .register(parameters: let parameters):
+            return .requestCustomJSONEncodable(parameters, encoder: encoder)
+        }
+    }
+    
+    var headers: [String : String]? {
+        return ["Content-type": "application/json"]
+    }
+}
---
+++ Shopping List/Shopping List/Networking/NetworkingConstants.swift
@@ -0,0 +1,12 @@
+//
+//  NetworkingConstants.swift
+//  Shopping List
+//
+//  Created by Mateusz Leśniara on 26/04/2021.
+//
+
+import Foundation
+
+enum NetworkingConstants {
+    static let baseURL = "http://projekt.shoplist.site:8080/api"
+}
